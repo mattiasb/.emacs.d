3e8dd686ad94d18486d32eb1dd48cb7a4a6f4167
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2014-08-25 23:04:56 +0200

    Scroll on compile

--- init.el
+++ init.el
@@ -380,6 +380,7 @@ optional packages."
  '(company-dabbrev-ignore-case t)
  '(company-idle-delay 0.5)
  '(company-tooltip-align-annotations t)
+ '(compilation-scroll-output t)
  '(cppcm-build-dirname "build")
  '(custom-enabled-themes (quote (wombat)))
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
