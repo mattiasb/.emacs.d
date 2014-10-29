87f71f8e8a2ca0bd74712dfe2fd91636545bc273
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2014-10-29 11:33:40 +0100

    Disable electric-layout-mode

--- init.el
+++ init.el
@@ -359,7 +359,6 @@
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(delete-selection-mode t)
  '(electric-indent-mode t)
- '(electric-layout-mode t)
  '(electric-pair-mode t)
  '(fci-rule-color "gray21")
  '(flycheck-completion-system (quote ido))
