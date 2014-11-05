b278020ac801b259be16bcfe9a76beb075bd2214
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2014-11-05 22:48:14 +0100

    Work around fci & company issue

--- init.el
+++ init.el
@@ -176,6 +176,24 @@
                               ("\C-g"    . company-abort)
                               ))))
 
+;; Workaround for fci-mode with Company
+;; https://github.com/company-mode/company-mode/issues/180#issuecomment-55047120
+(defvar-local company-fci-mode-on-p nil)
+
+(defun company-turn-off-fci (&rest ignore)
+  "Turn off fci-mode. (IGNORE any parameters)."
+  (when (boundp 'fci-mode)
+    (setq company-fci-mode-on-p fci-mode)
+    (when fci-mode (fci-mode -1))))
+
+(defun company-maybe-turn-on-fci (&rest ignore)
+  "Turn on fci-mode. (IGNORE any parameters)."
+  (when company-fci-mode-on-p (fci-mode 1)))
+
+(add-hook 'company-completion-started-hook   'company-turn-off-fci)
+(add-hook 'company-completion-finished-hook  'company-maybe-turn-on-fci)
+(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+
 ;; ELisp
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
