9c5eb87c6dc26bb6421a0bb4e1654fb1be50be2e
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-02-02 15:04:03 +0100

    Remove date command
    It is a snippet now

--- init.el
+++ init.el
@@ -123,7 +123,6 @@
    ( "C-c c"       .  my/toggle-comment)
    ( "C-c C-c"     .  my/toggle-programming-case-word-at-point)
    ( "C-c i"       .  insert-char)
-   ( "C-c d"       .  my/insert-date) ;; TODO: Replace with snippet
    ( "C-a"         .  mwim-beginning-of-code-or-line)
    ( "C-e"         .  mwim-end-of-code-or-line)
 
