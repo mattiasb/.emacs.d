0a3b9e550fa19853b7e622e0be7182232e1c2f4f
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-03-17 02:40:52 +0100

    Advice ido to use tramp if no perms

--- init.el
+++ init.el
@@ -533,6 +533,11 @@
   (when (null popup-instances)
     (my/fci-turn-on)))
 
+(defadvice ido-find-file (after find-file-sudo activate)
+  "Find file as root if necessary."
+  (unless (and buffer-file-name
+               (file-writable-p buffer-file-name))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 
 (provide 'init)
