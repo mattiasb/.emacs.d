bb2a7ddde78a4d6cf1124d959bbb79c97dc0aa7d
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-03-17 02:40:04 +0100

    Don't use company-ropemacs

--- custom.el
+++ custom.el
@@ -64,7 +64,7 @@
  '(company-auto-complete t)
  '(company-auto-complete-chars '(32 46))
  '(company-backends
-   '(company-elisp company-nxml company-css company-eclim company-semantic company-rtags company-clang company-xcode company-ropemacs company-cmake
+   '(company-elisp company-nxml company-css company-eclim company-semantic company-rtags company-clang company-xcode company-cmake
                    (company-gtags company-etags company-dabbrev-code company-keywords)
                    company-oddmuse company-files company-dabbrev))
  '(company-dabbrev-downcase nil)
