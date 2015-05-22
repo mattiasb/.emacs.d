0360dcc3f2314075fd33c27a2dc00c1352b9018b
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-05-22 04:40:43 +0200

    Use default aim mode settings

--- custom.el
+++ custom.el
@@ -43,8 +43,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aggressive-indent-excluded-modes
-   '(text-mode tabulated-list-mode special-mode minibuffer-inactive-mode bibtex-mode yaml-mode jabber-chat-mode lisp-interaction-mode))
  '(auto-insert 'other)
  '(auto-insert-alist
    '((("\\.el\\'" . "Emacs Lisp")
