a2462a3a883fd86d7dc47cb86f4c1f91843c767f
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2015-07-02 13:00:12 +0200

    Re-enable magit-gitflow

--- init.el
+++ init.el
@@ -371,11 +371,11 @@
             (fci-mode 1)
             (setq-local fill-column 72)))
 
-;; (add-hook 'magit-mode-hook
-;;           (lambda ()
-;;             (require 'magit-gitflow)
-;;             (when (fboundp 'turn-on-magit-gitflow)
-;;               (turn-on-magit-gitflow))))
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (require 'magit-gitflow)
+            (when (fboundp 'turn-on-magit-gitflow)
+              (turn-on-magit-gitflow))))
 
 ;; Markdown
 (defvar markdown-mode-map)
