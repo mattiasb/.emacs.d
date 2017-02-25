688463425448f0fd71619b29a444ef97be796a58
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2017-02-25 03:46:21 +0100

    Remove out commented code

--- my-after-init.el
+++ my-after-init.el
@@ -188,18 +188,6 @@
   (global-control-mode)
   (my/activate-god-mode-isearch))
 
-;; (defun my/activate-projectile-mode ()
-;;   "Activate Projectile Mode."
-;;   ;; This needs to be set before loading projectile
-;;   ;; and also can't be set from custom.el apparently
-;;   (setq projectile-mode-line
-;;         '(:eval (if (or (file-remote-p default-directory)
-;;                         (string-match-p "/run/user/[0-9]+/gvfs/"
-;;                                         default-directory))
-;;                     " [?]"
-;;                   (format " [%s]" (projectile-project-name)))))
-;;   (projectile-mode))
-
 (defun my/activate-modes ()
   "Activate a bunch of global modes."
   (unless (display-graphic-p)
