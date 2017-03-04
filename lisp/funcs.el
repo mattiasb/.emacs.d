044e080b8ef21e3e92d2af3763b80766232c700d
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2017-03-05 00:19:43 +0100

    Fix flycheck error

--- lisp/funcs.el
+++ lisp/funcs.el
@@ -519,7 +519,7 @@ depending on context."
   "Offer to create parent directory for current buffer if it doesn't exist."
   (let ((parent-directory (file-name-directory buffer-file-name)))
     (when (and (not (file-exists-p parent-directory))
-               (y-or-n-p (format "Directory `%s' does not exist! Create it?"
+               (y-or-n-p (format "Directory `%s' does not exist! Create it? "
                                  parent-directory)))
       (make-directory parent-directory t))))
 
