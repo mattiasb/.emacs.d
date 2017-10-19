9f7c2371f200394bdce85136b56cec87e0333a31
Mattias Bengtsson <mattias.bengtsson@smarteye.se>
2017-10-19 09:57:52 +0200

    Disable jshint

--- lisp/mb-hooks.el
+++ lisp/mb-hooks.el
@@ -415,6 +415,7 @@
   (defvar flimenu-imenu-separator)
   (setq-local flimenu-imenu-separator ".")
   (flycheck-disable-checker 'javascript-jscs)
+  (flycheck-disable-checker 'javascript-jshint)
   (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)
   (js2-imenu-extras-mode)
   (add-node-modules-path)
