f1dba4f94113bc996c7e554e555b4774de90cdbe
Mattias Bengtsson <mattias.bengtsson@smarteye.se>
2018-03-16 13:33:00 +0100

    Add Coding Game Haskell template

---
+++ templates/10:.*\.cg\.hs
@@ -0,0 +1,32 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
+module Test where
+
+int :: Integer -> Integer
+str :: String -> String
+ints :: [Integer] -> [Integer]
+strs :: [String] -> [String]
+
+strings :: String -> String
+strings  = (unlines . map string . lines)
+
+integers :: String -> String
+integers = (unlines . map (show . int . read) . lines)
+
+string :: String -> String
+string  = string
+
+integer :: String -> String
+integer = show . int . read
+
+main = interact format
+
+---------------------------
+
+-- One of integer(s) or string(s)
+format = integer
+
+int = (+ 1)
+str = id
+ints = map int
+strs = map str
