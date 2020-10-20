81e51374bfa80dc293261d68500f7701ae3498f0
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2020-10-20 18:29:49 +0200

    Disable: Toolbar and menubar in lisp
    This makes the toolbar and menubar disappear without flicker even
    without xresources.

---
+++ early-init.el
@@ -0,0 +1,38 @@
+;;; early-init.el --- Early Init -*- lexical-binding: t; -*-
+
+;; Copyright ⓒ 2013-2020 Mattias Bengtsson
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with This program.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Author: Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
+
+;; Version          : 20141020
+;; Keywords         : local
+;; Package-Requires : ((emacs "27.1"))
+;; URL              : https://github.com/moonlite/.emacs.d
+;; Compatibility    : GNU Emacs: 27.x
+
+;;; Commentary:
+
+;; My early init file
+
+;;; Note:
+
+;;; Code:
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+
+(provide 'early-init)
+;;; early-init.el ends here
