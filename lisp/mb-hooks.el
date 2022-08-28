8cf897bbe214066351df6de46085887fbda9bcac
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2022-08-28 04:57:23 +0200

    Electric-operator: Haskell workaround

--- lisp/mb-hooks.el
+++ lisp/mb-hooks.el
@@ -225,6 +225,8 @@ Based on: http://www.whiz.se/2016/05/01/dark-theme-in-emacs/"
                                         (cons "-" nil))
   (electric-operator-add-rules-for-mode 'makefile-gmake-mode
                                         (cons "-" nil))
+  (electric-operator-add-rules-for-mode 'haskell-mode
+                                        (cons "-" nil))
   (electric-operator-add-rules-for-mode 'sh-mode
                                         (cons "=" nil)))
 
