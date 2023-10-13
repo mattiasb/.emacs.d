cef662d4f19889025a353a6e6b4462c909ad679e
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2023-10-13 13:59:28 +0200

    dired: dired-dwim-target

--- lisp/mb-custom.el
+++ lisp/mb-custom.el
@@ -76,6 +76,7 @@
  '(custom-unlispify-tag-names nil)
  '(default-frame-alist '((vertical-scroll-bars) (fullscreen . maximized)))
  '(delete-selection-mode t)
+ '(dired-dwim-target 'dired-dwim-target-next)
  '(dired-listing-switches "-laGh1v --group-directories-first")
  '(dired-sidebar-width 40)
  '(display-fill-column-indicator-character 9474)
